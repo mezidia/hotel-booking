e610f2935288e0bb271de423faec69f713c4a113
Dmytrenko-Roman <54878089+Dmytrenko-Roman@users.noreply.github.com>
2020-09-07 12:00:34 +0300

    start project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace bd_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
