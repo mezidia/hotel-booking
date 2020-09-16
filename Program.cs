80af69b225e34da40ee7040df35994ceea15f475
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-09-16 15:42:50 +0000

    #4 Add solution file and delete another ones

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace booking_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
