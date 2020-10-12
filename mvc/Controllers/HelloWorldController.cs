d84046b2215f3df5f896b330adacfbe452edb3e4
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-10-12 16:32:52 +0000

    #7 #8 Add own template

--- mvc/Controllers/HelloWorldController.cs
+++ mvc/Controllers/HelloWorldController.cs
@@ -14,9 +14,9 @@ namespace mvc.Controllers
         // 
         // GET: /HelloWorld/
 
-        public string Index()
+        public IActionResult Index()
         {
-            return "This is my default action...";
+            return View();
         }
 
         // 
