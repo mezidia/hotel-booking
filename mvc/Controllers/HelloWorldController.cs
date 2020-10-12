2f66993dbe4d2b37ee84a1b3803a2791892cc09d
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-10-12 16:18:24 +0000

    #7 #8 Add new test Controller

---
+++ mvc/Controllers/HelloWorldController.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using mvc.Models;
+
+namespace mvc.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+    }
+}
