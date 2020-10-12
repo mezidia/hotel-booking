c7855b9b1a5c49e55d334aca213822f263fec0c7
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-10-12 06:38:23 +0000

    #8 Add "show" status code pages

--- mvc/Startup.cs
+++ mvc/Startup.cs
@@ -39,6 +39,8 @@ namespace mvc
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            
+            app.UseStatusCodePages();
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
