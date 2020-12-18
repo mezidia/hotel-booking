bc2dea7f2aab8fecc9ad12a2075561a1bb5ab121
lizardlynx <54850578+lizardlynx@users.noreply.github.com>
2020-12-18 12:06:01 +0200

    Add files via upload

---
+++ Database/Procedures/ChangePermissions.sql
@@ -0,0 +1,36 @@
+-- ================================================
+-- Template generated from Template Explorer using:
+-- Create Procedure (New Menu).SQL
+--
+-- Use the Specify Values for Template Parameters 
+-- command (Ctrl-Shift-M) to fill in the parameter 
+-- values below.
+--
+-- This block of comments will not be included in
+-- the definition of the procedure.
+-- ================================================
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [dbo].[ChangePermissions]
+	-- Add the parameters for the stored procedure here
+	@UserID int,
+	@Permissions int
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	UPDATE [dbo].[Users]
+	SET [permission_int] = @Permissions
+	WHERE [user_id] = @UserID
+END
+GO
