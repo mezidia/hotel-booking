c5c0619b5efc7b2da5d7f2ccd6494fd4de8adbd3
lizardlynx <54850578+lizardlynx@users.noreply.github.com>
2020-12-19 20:49:30 +0200

    Rename UpdateHotelInfo.sql to 2020_12_14_13_SP_UpdateHotelInfo.sql

---
+++ Database/Procedures/2020_12_14_13_SP_UpdateHotelInfo.sql
@@ -0,0 +1,35 @@
+﻿USE [Hotels_DB]
+GO
+/****** Object:  StoredProcedure [dbo].[UpdateHotelInfo]    Script Date: 14.12.2020 12:35:29 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+ALTER PROCEDURE [dbo].[UpdateHotelInfo]
+	-- Add the parameters for the stored procedure here
+	@HotelID int,
+	@NewOwner int,
+	@Description nvarchar(max),
+	@Location nvarchar(max),
+	@HotelType int,
+	@HotelName nvarchar(max)
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+    -- Insert statements for procedure here
+
+	UPDATE [dbo].[Hotels]
+	SET [owner_id] = @NewOwner,
+		[description_str] = @Description,
+		[location_int] = @Location,
+		[hotelType_int] = @HotelType,
+		[hotelName_str] = @HotelName
+	WHERE [hotel_id] = @HotelID
+END
