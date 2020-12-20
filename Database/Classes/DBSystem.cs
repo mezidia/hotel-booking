fb13569a8d7b1ae4a57348a0d91d05b28781be95
La7rodectus <54902922+La7rodectus@users.noreply.github.com>
2020-12-20 20:26:23 +0200

    add new methods (#33)

--- Database/Classes/DBSystem.cs
+++ Database/Classes/DBSystem.cs
@@ -19,11 +19,62 @@ namespace Hotel_booking
 		{
 
 		}
-
-		public void GetUser()
+        //input: id of user
+        //output: object[country, permission, phoneNumber, email, login, userName, age, password]
+        public object[] GetUser(int id)
 		{
+            SqlConnection conn = DBConnConfig.GetDBConnection();
+            conn.Open();
 
-		}
+            object[] UserData = new object[8];
+
+            try
+            {
+                SqlCommand cmd = new SqlCommand("GetUser", conn);
+
+                //Command type -> StoredProcedure
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add("@UserID", SqlDbType.Int).Value = id;
+
+                //exec procedure
+                cmd.ExecuteNonQuery();
+
+                using (SqlDataReader rdr = cmd.ExecuteReader())
+                {
+                    while (rdr.Read())
+                    {
+                        UserData[0] = int.Parse(rdr["country_id"].ToString());
+                        UserData[1] = int.Parse(rdr["permission_int"].ToString());
+                        UserData[2] = int.Parse(rdr["phoneNumber_int"].ToString());
+                        UserData[3] = rdr["email_str"].ToString();
+                        UserData[4] = rdr["login_str"].ToString();
+                        UserData[5] = rdr["userName_str"].ToString();
+                        UserData[6] = int.Parse(rdr["age_int"].ToString());
+                        UserData[7] = rdr["password_str"].ToString();
+                    }
+                }
+                //output recieved data
+                Console.WriteLine("country id : " + UserData[0]);
+                Console.WriteLine("permission : " + UserData[1]);
+                Console.WriteLine("phoneNumber : " + UserData[2]);
+                Console.WriteLine("email : " + UserData[3]);
+                Console.WriteLine("userName : " + UserData[4]);
+                Console.WriteLine("hotel type: " + UserData[5]);
+                Console.WriteLine("age : " + UserData[6]);
+                Console.WriteLine("password : " + UserData[7]);
+            }
+            catch (Exception e)
+            {
+                //Console.log error
+                Console.WriteLine("Error: " + e.Message);
+            }
+
+            //close conn
+            conn.Close();
+            conn.Dispose();
+
+            return UserData;
+        }
 
         //input: id of hotel
         //output: object[country, owner id, rating, description, location, hotel type, hotel name]
@@ -31,40 +82,53 @@ namespace Hotel_booking
         {
             SqlConnection conn = DBConnConfig.GetDBConnection();
             conn.Open();
-            SqlCommand cmd = new SqlCommand("GetHotel", conn);
-
-            // Вид Command является StoredProcedure
-            cmd.CommandType = CommandType.StoredProcedure;
-            cmd.Parameters.Add("@HotelID", SqlDbType.Int).Value = id;
-
-            // Выполнить процедуру.
-            cmd.ExecuteNonQuery();
 
             object[] HotelData = new object[8];
 
-            using (SqlDataReader rdr = cmd.ExecuteReader())
+            try
             {
-                while (rdr.Read())
+                SqlCommand cmd = new SqlCommand("GetHotel", conn);
+
+                //Command type -> StoredProcedure
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add("@HotelID", SqlDbType.Int).Value = id;
+
+                //exec procedure
+                cmd.ExecuteNonQuery();
+
+                using (SqlDataReader rdr = cmd.ExecuteReader())
                 {
-                    HotelData[0] = int.Parse(rdr["country_id"].ToString());
-                    HotelData[1] = int.Parse(rdr["owner_id"].ToString());
-                    HotelData[2] = int.Parse(rdr["number_of_stars_int"].ToString());
-                    HotelData[3] = rdr["description_str"].ToString();
-                    HotelData[4] = int.Parse(rdr["location_int"].ToString());
-                    HotelData[5] = int.Parse(rdr["hotelType_int"].ToString());
-                    HotelData[6] = int.Parse(rdr["rating_int"].ToString());
-                    HotelData[7] = rdr["hotelName_str"].ToString();
+                    while (rdr.Read())
+                    {
+                        HotelData[0] = int.Parse(rdr["country_id"].ToString());
+                        HotelData[1] = int.Parse(rdr["owner_id"].ToString());
+                        HotelData[2] = int.Parse(rdr["number_of_stars_int"].ToString());
+                        HotelData[3] = rdr["description_str"].ToString();
+                        HotelData[4] = int.Parse(rdr["location_int"].ToString());
+                        HotelData[5] = int.Parse(rdr["hotelType_int"].ToString());
+                        HotelData[6] = int.Parse(rdr["rating_int"].ToString());
+                        HotelData[7] = rdr["hotelName_str"].ToString();
+                    }
                 }
+                //output recieved data
+                Console.WriteLine("country: " + HotelData[0]);
+                Console.WriteLine("owner id: " + HotelData[1]);
+                Console.WriteLine("number of stars: " + HotelData[2]);
+                Console.WriteLine("description: " + HotelData[3]);
+                Console.WriteLine("location: " + HotelData[4]);
+                Console.WriteLine("hotel type: " + HotelData[5]);
+                Console.WriteLine("rating: " + HotelData[6]);
+                Console.WriteLine("hotel name: " + HotelData[7]);
+            } catch (Exception e)
+            {
+                //Console.log error
+                Console.WriteLine("Error: " + e.Message);
             }
-            //output recieved data
-            Console.WriteLine("country: " + HotelData[0]);
-            Console.WriteLine("owner id: " + HotelData[1]);
-            Console.WriteLine("number of stars: " + HotelData[2]);
-            Console.WriteLine("description: " + HotelData[3]);
-            Console.WriteLine("location: " + HotelData[4]);
-            Console.WriteLine("hotel type: " + HotelData[5]);
-            Console.WriteLine("rating: " + HotelData[6]);
-            Console.WriteLine("hotel name: " + HotelData[7]);
+
+            //close conn
+            conn.Close();
+            conn.Dispose();
+
             return HotelData;
         }
     }
