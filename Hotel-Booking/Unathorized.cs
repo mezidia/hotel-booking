321cf31dbda5ebb6e16e0e90076aea3007b0de37
VsIG <Valentyn2202@gmail.com>
2020-12-20 11:31:06 +0200

    ✨: create Unathorized user

---
+++ Hotel-Booking/Unathorized.cs
@@ -0,0 +1,14 @@
+using System;
+using hotel_booking.SqlConn;
+using System.Data.SqlClient;
+
+namespace Hotel_booking
+{
+	public class Unathorized : User
+	{ 
+		public void  SignUpLogIn()
+		{
+
+		}
+	}
+}
