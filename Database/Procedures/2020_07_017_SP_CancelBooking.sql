e5d980fd3b19718666492dc08775952a2b9f9c06
La7rodectus <54902922+La7rodectus@users.noreply.github.com>
2020-12-20 15:58:09 +0200

    Rename 2020_07_12_SP_CancelBooking.sql to 2020_07_017_SP_CancelBooking.sql

---
+++ Database/Procedures/2020_07_017_SP_CancelBooking.sql
@@ -0,0 +1,23 @@
+﻿USE [Hotels_DB]
+GO
+/****** Object:  StoredProcedure [dbo].[LogIn]    Script Date: 07.12.2020 9:33:55 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [dbo].[CancelBooking] 
+@Book  int
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+SELECT [book_id], [user_id] 
+
+  FROM [dbo].[Booking] WHERE book_id = @Book
+END
