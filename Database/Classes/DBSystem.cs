9b7e7723d842c1eb57488f83b6ca7828c9c731ce
VsIG <Valentyn2202@gmail.com>
2020-12-20 14:36:15 +0200

    Create DBSystem.cs

---
+++ Database/Classes/DBSystem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hotel_booking
+{
+	class DBSystem
+	{
+		// methods
+		public void GetReviews()
+		{
+
+		}
+
+		public void GetRooms()
+		{
+
+		}
+
+		public void GetUser()
+		{
+
+		}
+
+		public void GetHotel()
+		{
+
+		}
+	}
+}
