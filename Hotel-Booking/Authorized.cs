874d3d5910e014c97a7903d9c007b892b8cd8fad
VsIG <Valentyn2202@gmail.com>
2020-12-20 11:43:48 +0200

    ✨: classes update

---
+++ Hotel-Booking/Authorized.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using hotel_booking.SqlConn;
+using System.Data.SqlClient;
+
+namespace Hotel_booking
+{
+	public class Authorized : User
+	{
+		protected string email;
+		protected string login;
+		protected string password;
+
+		protected int age;
+		protected int phoneNumber;
+		protected int country;
+		protected int id;
+	}
+}
---
+++ Hotel-Booking/Unauthorized.cs
@@ -0,0 +1,14 @@
+using System;
+using hotel_booking.SqlConn;
+using System.Data.SqlClient;
+
+namespace Hotel_booking
+{
+	public class Unauthorized : User
+	{ 
+		public void  SignUpLogIn()
+		{
+
+		}
+	}
+}
