a40859414096fbae48a0868b278536bea1f9b761
VsIG <Valentyn2202@gmail.com>
2020-12-20 11:31:11 +0200

    Update User.cs

--- Hotel-Booking/User.cs
+++ Hotel-Booking/User.cs
@@ -4,5 +4,27 @@ using System.Data.SqlClient;
 
 namespace Hotel_booking
 {
-	class
+	class User
+	{
+		public int CheckRooms()
+		{
+
+
+			return 0;
+		}
+
+		public int CheckDeals()
+		{
+
+
+			return 0;
+		}
+
+		public int Search()
+		{
+
+
+			return 0;
+		}
+	}
 }
