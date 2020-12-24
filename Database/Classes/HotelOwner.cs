7818f746336dc0512e2a68a61f125e7f80105928
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-12-24 11:49:45 +0200

    Update HotelOwner.cs

--- Database/Classes/HotelOwner.cs
+++ Database/Classes/HotelOwner.cs
@@ -7,9 +7,7 @@ namespace Hotel_booking
     public class HotelOwner : HotelAdmin
     {
         // methods
-        public void ChangeAccess(int user_id, int country_id, int phoneNumber_int,
-            string email_str, string login_str, string userName_str, 
-            int age_int, string password_str, int permission_int)
+        public void ChangeAccess(object[] hotelfields)
         {
             SqlConnection conn = DBConnConfig.GetDBConnection();
             conn.Open();
@@ -17,15 +15,15 @@ namespace Hotel_booking
 
             // Вид Command является StoredProcedure
             cmd.CommandType = CommandType.StoredProcedure;
-            cmd.Parameters.Add("@UserID", SqlDbType.Int).Value = user_id;
-            cmd.Parameters.Add("@PhoneNumber", SqlDbType.Int).Value = phoneNumber_int;
-            cmd.Parameters.Add("@Age", SqlDbType.Int).Value = age_int;
-            cmd.Parameters.Add("@Country", SqlDbType.Int).Value = country_id;
-            cmd.Parameters.Add("@Permission", SqlDbType.Int).Value = permission_int;
-            cmd.Parameters.Add("@UserName", SqlDbType.NVarChar, int.MaxValue).Value = userName_str;
-            cmd.Parameters.Add("@Login", SqlDbType.NVarChar, int.MaxValue).Value = login_str;
-            cmd.Parameters.Add("@Email", SqlDbType.NVarChar, int.MaxValue).Value = email_str;
-            cmd.Parameters.Add("@Password", SqlDbType.NVarChar, int.MaxValue).Value = password_str;
+            cmd.Parameters.Add("@UserID", SqlDbType.Int).Value = hotelfields[0];
+            cmd.Parameters.Add("@PhoneNumber", SqlDbType.Int).Value = hotelfields[1];
+            cmd.Parameters.Add("@Age", SqlDbType.Int).Value = hotelfields[2];
+            cmd.Parameters.Add("@Country", SqlDbType.Int).Value = hotelfields[3];
+            cmd.Parameters.Add("@Permission", SqlDbType.Int).Value = hotelfields[4];
+            cmd.Parameters.Add("@UserName", SqlDbType.NVarChar, int.MaxValue).Value = hotelfields[5];
+            cmd.Parameters.Add("@Login", SqlDbType.NVarChar, int.MaxValue).Value = hotelfields[6];
+            cmd.Parameters.Add("@Email", SqlDbType.NVarChar, int.MaxValue).Value = hotelfields[7];
+            cmd.Parameters.Add("@Password", SqlDbType.NVarChar, int.MaxValue).Value = hotelfields[8];
             // Выполнить процедуру.
             cmd.ExecuteNonQuery();
             conn.Close();
